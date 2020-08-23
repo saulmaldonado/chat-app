5b72a387536a1f7f72e73c78eb7b21165506bdba
Saul Maldonado <samaldonado4@gmail.com>
2020-08-23 12:06:56 -0500

    Add options tooltip

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,21 +1,22 @@
 <template>
   <header class="navigation">
-    <img
-      src="../assets/images/chevron-left-solid.svg"
-      height="15"
-      width="7.5"
-      class="left-chevron"
-    />
-    <img
-      src="../assets/images/avatar.jpg"
-      alt="avatar"
-      class="avatar"
-    />
+    <img src="../assets/images/chevron-left-solid.svg" height="15" width="7.5" class="left-chevron" />
+    <img src="../assets/images/avatar.jpg" alt="avatar" class="avatar" />
     <div class="recipient-info">
       <p class="name">{{ name }}</p>
       <p class="status">{{ status }}</p>
     </div>
-    <div class="options"></div>
+    <div class="options" tabindex="0">
+      <div class="tooltip">
+        <a href="https://github.com/saulmaldonado" rel="noopener" target="_blank">
+          <img src="../assets/images/github-brands.svg" alt="github" />
+        </a>
+
+        <a href="https://saulmaldonado.tech" rel="noopener" target="_blank">
+          <img src="../assets/images/user-alt-solid.svg" alt="portfolio" />
+        </a>
+      </div>
+    </div>
   </header>
 </template>
 
